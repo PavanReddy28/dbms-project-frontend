7320c23e8825de0d9093b10194f18e15a9886a18
Yash <yashyenugu@gmail.com>
2021-03-24 21:42:15 +0530

    Added basic information

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Grid, Card, CardHeader, CardContent } from "@material-ui/core";
 
 function Home() {
+
+    const featureCards = [
+        {
+            heading: "Register Players",
+            content: "Add new players which play in different sports"
+        },
+        {
+            heading: "Schedule matches",
+            content: "Set matches for different sports without clashes"
+        },
+        {
+            heading: "Variety of sports",
+            content: "The application offers features for various sports like football, tennis, basketball etc."
+        }
+    ]
     return (
         // <Grid container justify="center">
         //     <Grid item>
@@ -10,9 +25,53 @@ function Home() {
 
         // </Grid>
         <>
-        <Typography variant="h4" align="center">Welcome to the Tournament Management App!</Typography>
-        <Typography variant="subtitle1" align="center">An app to help with your tournament hassle</Typography>
-        <Typography variant="body1" align="center">We offer an app to help you manage tournaments</Typography>
+            <Typography variant="h3" align="center">Welcome to the Tournament Management App!</Typography>
+            <Typography variant="subtitle1" align="center">An app to help with your tournament hassle</Typography>
+            <Typography variant="body1" align="center">We offer an app to help you manage tournaments</Typography>
+            <hr></hr>
+            <div>
+                <Typography variant="h3" align="center">Features Offered</Typography>
+                <Grid container alignItems="flex-end">
+                    {/* <Grid item>
+                    <Card>
+                        <CardHeader title="Register Players" titleTypographyProps={{ align: 'center' }} />
+                        <CardContent>
+                            <Typography>Add new players which play in different sports</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card>
+                        <CardHeader title="Register Players" titleTypographyProps={{ align: 'center' }} />
+                        <CardContent>
+                            <Typography>Add new players which play in different sports</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item>
+                    <Card>
+                        <CardHeader title="Register Players" titleTypographyProps={{ align: 'center' }} />
+                        <CardContent>
+                            <Typography>Add new players which play in different sports</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid> */}
+
+                    {featureCards.map((item,index) => {
+                        return (
+                            <Grid item key={index} sm={12} md={4} lg={4}>
+                                <Card>
+                                    <CardHeader title={item.heading} titleTypographyProps={{ align: 'center' }} />
+                                    <CardContent>
+                                        <Typography>{item.content}</Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        )
+                    })}
+
+                </Grid>
+            </div>
         </>
 
     )
