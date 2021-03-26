e00f8dc2fa96548e150d6732804659063ed002ea
bot977 <ypavan2802@gmail.com>
2021-03-26 22:48:14 +0530

    Sub-Component 3 - Review of the detials entered soo far.

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,45 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, List, ListItem, ListItemText, Grid } from '@material-ui/core'
-
-const Tournament = [
-    {
-        id: 1,
-        general : {
-            tournName: "Arena",
-            organizer: "BITS Pilani Hyderabad",
-            location: "Hyderabad, Telangana"
-        },
-        sports : {
-            teamSports: [
-                'Basketball',
-                'Cricket'
-            ],
-            indivSports: [
-                'Tennis',
-                'Badminton'
-            ]
-        }        
-    },
-    {
-        id: 2,
-        general : {
-            tournName: "Spree",
-            organizer: "BITS Pilani Goa",
-            location : "Goa"
-        },
-        sports : {
-            teamSports: [
-                'Basketball',
-                'Soccer',
-            ],
-            indivSports: [
-                'Tennis',
-                'Table Tennis',
-            ]
-        }
-    }
-]
+import { Typography, Grid, Button } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
     listItem: {
@@ -51,38 +12,67 @@ const useStyles = makeStyles((theme) => ({
     title: {
         marginTop: theme.spacing(2),
     },
+    buttons: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    button: {
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+    },
 }));
 
-const Review = () => {
+const Review = ({ Tournament, sports, handleNext, handleBack }) => {
 
     const classes = useStyles();
 
+    const onSubmit = (e) => {
+        handleNext();
+    };
+
+    const onhSubmit = (e) => {
+        handleBack();
+    };
+
     return (
         <React.Fragment>
-            <Typography variant="h6" gutterBottom>
-                Tournament Summary
+            <Typography variant="h5" gutterBottom>
+                Review
             </Typography>
-            <List disablePadding>
-                <ListItem className={classes.listItem} key={Tournament[0].id}>
-                    <ListItemText primary={Tournament[0].general.tournName} secondary={Tournament[0].general.organizer} />
-                    <Typography variant="body2">{}</Typography>
-                </ListItem>
-            </List>
+            <Grid item xs={12}>
+                    <Typography variant="h6" gutterBottom className={classes.title}>
+                    {Tournament[0].tournName}
+                    </Typography>
+                    <Typography variant="body2">{Tournament[0].college}</Typography>
+                </Grid>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
                     <Typography variant="h6" gutterBottom className={classes.title}>
                         Location
                     </Typography>
-                    <Typography gutterBottom>{Tournament[0].general.organizer}</Typography>
-                    <Typography gutterBottom>{Tournament[0].general.location}</Typography>
+                    <Typography gutterBottom>{Tournament[0].college}</Typography>
+                    <Typography gutterBottom>{Tournament[0].address}</Typography>
+                    <Typography gutterBottom>{Tournament[0].city}</Typography>
                 </Grid>
                 <Grid item container direction="column" xs={12} sm={6}>
                     <Typography variant="h6" gutterBottom className={classes.title}>
                         Sports Being Organized
                     </Typography>
-                    <Typography gutterBottom>{Tournament[0].sports.teamSports.join(', ')}</Typography>
-                    <Typography gutterBottom>{Tournament[0].sports.indivSports.join(', ')}</Typography>                            
+                    <Typography gutterBottom>{sports[0].teamSport.join(', ')}</Typography>
+                    <Typography gutterBottom>{sports[0].indivSport.join(', ')}</Typography>                            
                 </Grid>
+                <div className={classes.buttons}>
+                    <Button onClick={onhSubmit} className={classes.button}>
+                        Back
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={onSubmit}
+                        className={classes.button}>
+                        Create
+                    </Button>
+                </div>
             </Grid>
         </React.Fragment>
     )
