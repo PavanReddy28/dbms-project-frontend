8d0bf3f379c7e6adc6bd7c1fc36479520372395a
Yash <yashyenugu@gmail.com>
2021-03-28 16:43:19 +0530

    Added integration with GET request to get all tournaments

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { Grid, Container, Typography, makeStyles, Paper, List, ListItem, ListItemText } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { axiosInstance } from "../axiosInstance"
+import { Grid, Container, Typography, makeStyles, Paper, List, ListItem, ListItemText, Button, IconButton } from '@material-ui/core';
+// import { DeleteIcon } from "@material-ui/icons";
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
 
 const useStyles = makeStyles(() => ({
     paper: {
@@ -10,29 +15,64 @@ const useStyles = makeStyles(() => ({
     },
     list: {
         maxHeight: "300px",
-        overflow:"auto"
+        overflow: "auto"
+    },
+    addIcon: {
+        color: "green",
+        backgroundColor: "blue"
     }
 }))
 
 function Dashboard() {
 
     const classes = useStyles();
-    const tournaments = ["ARENA", "BLITZKRIEG", "BOSM", "ARENA", "BLITZKRIEG", "BOSM", "ARENA", "BLITZKRIEG", "BOSM", "ARENA", "BLITZKRIEG", "BOSM"];
+    const [tournaments, setTournaments] = useState([]);
     const matches = ["Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd"];
 
+    useEffect(() => {
+        axiosInstance.get("/tournament", {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        }).then(response => {
+            setTournaments(response.data.tournaments);
+            // console.log(response.data.tournaments)
+        });
+    }, [])
+
+    // axiosInstance.get("/tournament",{
+    //             headers: {
+    //                 "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+    //             }
+    //         }).then(response => setTournaments(response.data.tournaments))
+    //         .catch(err => console.log(err))
 
     return (
-        <Grid container spacing = {2} className = {classes.container}>
+        <Grid container spacing={2} className={classes.container}>
 
             <Grid item lg={6} sm={12}>
                 <Paper className={classes.paper}>
                     <Typography variant="h5" color="primary">Current Tournaments</Typography>
-                    <List className = {classes.list}>
+                    <IconButton>
+                        <AddIcon />
+                    </IconButton>
 
-                    
-                    {tournaments.map(tournament => {
-                        return <ListItem><ListItemText primary={tournament} /></ListItem>
-                    })}
+                    <List className={classes.list}>
+
+
+                        {tournaments.map(tournament => {
+                            return (
+                                <ListItem key={tournament.tournament_id}>
+                                    <ListItemText primary={tournament.t_name} />
+                                    <IconButton size="small">
+                                        <EditIcon /> 
+                                    </IconButton>
+                                    <IconButton size="small">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </ListItem>
+                            )
+                        })}
                     </List>
                 </Paper>
             </Grid>
@@ -40,12 +80,12 @@ function Dashboard() {
             <Grid item lg={6} sm={12}>
                 <Paper className={classes.paper}>
                     <Typography variant="h5" color="primary">Current Matches</Typography>
-                    <List className = {classes.list}>
+                    <List className={classes.list}>
 
-                    
-                    {matches.map(match => {
-                        return <ListItem><ListItemText primary={match} /></ListItem>
-                    })}
+
+                        {matches.map(match => {
+                            return <ListItem><ListItemText primary={match} /></ListItem>
+                        })}
                     </List>
                 </Paper>
             </Grid>
