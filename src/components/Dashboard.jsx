16edd42649abf11f4c5ac0d71c3bfe6a06b454ad
Yash <yashyenugu@gmail.com>
2021-03-28 23:40:57 +0530

    Added deletion of tournaments

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { axiosInstance } from "../axiosInstance"
-import { Grid, Container, Typography, makeStyles, Paper, List, ListItem, ListItemText, Button, IconButton, Collapse } from '@material-ui/core';
+import { 
+    Grid,
+    Container, 
+    Typography, 
+    makeStyles, 
+    Paper, 
+    List, 
+    ListItem, 
+    ListItemText, 
+    Button, 
+    IconButton, 
+    Collapse,
+    Dialog,
+    DialogActions,
+    DialogContentText,
+    DialogTitle,
+    DialogContent 
+} from '@material-ui/core';
 // import { DeleteIcon } from "@material-ui/icons";
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
@@ -35,6 +52,8 @@ function Dashboard() {
     const classes = useStyles();
     const [tournaments, setTournaments] = useState([]);
     const [open, setOpen] = useState(null)
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [dialogTourn, setDialogTourn] = useState(null);
     const matches = ["Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd", "Goa vs Pilani", "Goa vs Hyd"];
 
     useEffect(() => {
@@ -58,15 +77,41 @@ function Dashboard() {
         id !== open?setOpen(id) : setOpen(null);
     }
 
-    function handleDelete(id) {
-        setTournaments(previous => {
-            return previous.filter(item => {
-                return item.tournament_id !== id
-            })
+    function handleConfirm(tournament) {
+        // setTournaments(previous => {
+        //     return previous.filter(item => {
+        //         return item.tournament_id !== id
+        //     })
+        // })
+        setDialogTourn(tournament);
+        setDialogOpen(true);
+    }
+
+    function HandleDelete() {
+        axiosInstance.delete("/tournament", {
+            data:{
+                "tournament_id":dialogTourn.tournament_id
+            },
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+            }
         })
+        setTournaments(previous => {
+            return previous.filter( tournament => {
+                return tournament.tournament_id !== dialogTourn.tournament_id
+            })
+        });
+        setDialogOpen(false);
+        setDialogTourn(null);
+    }
+
+    function handleCancel() {
+        setDialogTourn(null);
+        setDialogOpen(false);
     }
 
     return (
+        <>
         <Grid container spacing={2} className={classes.container}>
 
             <Grid item lg={6} sm={12}>
@@ -88,7 +133,7 @@ function Dashboard() {
                                         <EditIcon /> 
                                     </IconButton>
                                     <IconButton size="small">
-                                        <DeleteIcon onClick = {() => handleDelete(tournament.tournament_id)}/>
+                                        <DeleteIcon onClick = {() => handleConfirm(tournament)}/>
                                     </IconButton>
                                     {open === tournament.tournament_id ? <ExpandLess /> : <ExpandMore />}
                                 </ListItem>
@@ -123,11 +168,29 @@ function Dashboard() {
                     </List>
                 </Paper>
             </Grid>
-
-
-
-
         </Grid>
+        <Dialog
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{dialogTourn !== null?"Delete tournament '" + dialogTourn.t_name + "' ?":""}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            By pressing confirm this tournament will be permanently deleted
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={HandleDelete} color="secondary" >
+            Confirm
+          </Button>
+          <Button onClick={handleCancel} color="primary" autoFocus>
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+        </>
     )
 }
 
