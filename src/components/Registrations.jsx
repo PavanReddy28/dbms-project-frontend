0ba0fda82948a0e455a7b8d2008b441526ed2c20
Yash <yashyenugu@gmail.com>
2021-04-04 11:18:13 +0530

    Added integration with backend

--- src/components/Registrations.jsx
+++ src/components/Registrations.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Paper, makeStyles, List, ListItem, ListItemText, Divider, Button, Grid, IconButton, Collapse } from "@material-ui/core";
+import { Typography, Paper, makeStyles, List, ListItem, ListItemText, Divider, Button, Grid, IconButton, Collapse, Dialog, DialogContent, DialogContentText, DialogActions } from "@material-ui/core";
 import CheckIcon from '@material-ui/icons/Check';
 import ClearIcon from '@material-ui/icons/Clear';
 import ExpandLess from '@material-ui/icons/ExpandLess';
@@ -23,55 +23,107 @@ const RegistrationStyles = makeStyles((theme) => ({
     }
 }))
 
-function RegistrationList({data, title}) {
+function RegistrationList({ data, title }) {
 
     const classes = RegistrationStyles()
 
-    const [open,setOpen] = useState(null);
+    const [open, setOpen] = useState(null);
+
+    const [dialogReg, setDialogReg] = useState(null);
+    const [registerDialog, setRegisterDialog] = useState(false);
+    const [rejectDialog, setRejectDialog] = useState(false);
 
     function handleCollapse(id, type) {
         id !== open ? setOpen(id) : setOpen(null)
     }
 
+    function setStatus(status) {
+        console.log(dialogReg);
+        axiosInstance.put("/team", {
+            tournament_id: dialogReg.tournament_id,
+            team_id: dialogReg.team_id,
+            status_update_to: status
+        }, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+            } 
+        }).then(response => {
+            setDialogReg(null);
+            window.location.reload(false);
+        });
+    }
+
     return (
-        <Paper elevation={1} className={classes.paper}>
-                    <Typography variant="h5" color="primary">{title}</Typography>
-                    <List>
-                        {Object.keys(data).length !== 0 ? Object.keys(data).map(tournament => {
+        <>
+            <Dialog open={registerDialog} onClose={() => setRegisterDialog(false)}>
+                <DialogContentText>Register the team?</DialogContentText>
+                <DialogActions>
+                    <Button onClick={() => setStatus("REGISTERED")} color="primary" >
+                        Confirm
+                    </Button>
+                    <Button onClick={() => setRegisterDialog(false)} color="primary" autoFocus>
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog open={rejectDialog} onClose={() => setRejectDialog(false)}>
+            <DialogContentText>Reject the team?</DialogContentText>
+                <DialogActions>
+                    <Button onClick={() => setStatus("REJECTED")} color="primary" >
+                        Confirm
+                    </Button>
+                    <Button onClick={() => setRejectDialog(false)} color="primary" autoFocus>
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Paper elevation={1} className={classes.paper}>
+                <Typography variant="h5" color="primary">{title}</Typography>
+                <List>
+                    {Object.keys(data).length !== 0 ? Object.keys(data).map(tournament => {
 
 
-                            return (
-                                <>
-                                    <ListItem button key={tournament} onClick={() => handleCollapse(tournament, "pending")}>
-                                        <ListItemText primary={tournament} />
-                                        {open === tournament ? <ExpandLess /> : <ExpandMore />}
-                                    </ListItem>
-                                    <Collapse in={open === tournament} timeout="auto">
-                                        <List component="div" disablePadding>
-                                            {data[tournament].map(reg => {
-                                                return (
-                                                    <>
-                                                        <ListItem key={reg.team_id}>
-                                                            <ListItemText primary={reg.team_name} secondary={tournament} className={classes.nested} />
-                                                            <IconButton size="small" className={classes.checkIcon}>
-                                                                <CheckIcon />
-                                                            </IconButton>
-                                                            <IconButton size="small" color="secondary">
-                                                                <ClearIcon />
-                                                            </IconButton>
-                                                        </ListItem>
-                                                        <Divider />
-                                                    </>
-                                                )
-                                            })}
-                                        </List>
-                                    </Collapse>
-                                </>
-                            )
+                        return (
+                            <>
+                                <ListItem button key={tournament} onClick={() => handleCollapse(tournament, "pending")}>
+                                    <ListItemText primary={tournament} />
+                                    {open === tournament ? <ExpandLess /> : <ExpandMore />}
+                                </ListItem>
+                                <Collapse in={open === tournament} timeout="auto">
+                                    <List component="div" disablePadding>
+                                        {data[tournament].map(reg => {
+                                            return (
+                                                <>
+                                                    <ListItem key={reg.team_id}>
+                                                        <ListItemText primary={reg.team_name} secondary={tournament} className={classes.nested} />
+                                                        <IconButton size="small" className={classes.checkIcon} onClick={() => {
+                                                            setDialogReg(reg);
+                                                            setRegisterDialog(true);
+                                                            
+                                                            }}>
+                                                            <CheckIcon />
+                                                        </IconButton>
+                                                        <IconButton size="small" color="secondary" onClick={() => {
+                                                            setDialogReg(reg);
+                                                            setRejectDialog(true);
+                                                            
+                                                            }}>
+                                                            <ClearIcon />
+                                                        </IconButton>
+                                                    </ListItem>
+                                                    <Divider />
+                                                </>
+                                            )
+                                        })}
+                                    </List>
+                                </Collapse>
+                            </>
+                        )
 
-                        }) : <Typography>No registrations</Typography>}
-                    </List>
-                </Paper>
+                    }) : <Typography>No registrations</Typography>}
+                </List>
+            </Paper>
+        </>
     )
 }
 
@@ -109,21 +161,21 @@ function Registrations() {
         })
     }, [])
 
-    
+
 
     return (
         <Grid container spacing={1} className={classes.container}>
 
             <Grid item sm={12}>
-                <RegistrationList data={pending} title="Pending Registrations"/>
+                <RegistrationList data={pending} title="Pending Registrations" />
             </Grid>
 
             <Grid item sm={12}>
-                <RegistrationList data={registered} title="Registered Teams"/>
+                <RegistrationList data={registered} title="Registered Teams" />
             </Grid>
 
             <Grid item sm={12}>
-                <RegistrationList data={rejected} title = "Rejected Teams"/>
+                <RegistrationList data={rejected} title="Rejected Teams" />
             </Grid>
 
 
