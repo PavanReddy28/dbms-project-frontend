9aee33caec114b1c0402c1ea2038d7c9a0442a23
Pavan Kumar Reddy <56450475+bot977@users.noreply.github.com>
2021-04-06 07:24:58 +0530

    parent component of addMatch

--- src/components/Matches/AddMatch.jsx
+++ src/components/Matches/AddMatch.jsx
@@ -4,24 +4,12 @@ import {Alert} from "@material-ui/lab";
 import {Link} from "react-router-dom";
 import { 
     CssBaseline,
-    Grid,
     Typography,
     Paper,
-    List,
-    ListItem,
-    ListItemText,
-    Button,
-    IconButton,
-    Divider,
-    Collapse, } from '@material-ui/core'
+    Button, } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { useHistory } from "react-router-dom";
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
 import AM_Tourn from './AM_Tourn'
 import AM_teams from './AM_teams'
-import AM_time from './AM_time'
 
 const useStyles = makeStyles((theme) => ({
     layout: {
@@ -49,10 +37,14 @@ const useStyles = makeStyles((theme) => ({
 const AddMatch = () => {
 
     const classes = useStyles();
-    const [Matches, setMatches] = useState([])
     const [Tournaments, setTournaments] = useState([])
     const [activeStep, setActiveStep] = useState(0);
     const [Teams, setTeams] = useState([])
+    const [tourn, settourn] = useState({})
+    const [mTeams, setmTeams] = useState([])
+    const [sport, setsport] = useState('')
+    const [time, setTime] = useState('')
+    const [round, setRound] = useState('')
 
     useEffect(() => {
         
@@ -67,22 +59,17 @@ const AddMatch = () => {
     }, [])
 
     const addTeams = (tourn1, sportName) => {
-        console.log(tourn1)
-        console.log(sportName)
-        axiosInstance.get('/sport/teams', {
-            params:{
-                'tournament_id' : tourn1.tournament_id,
-                'sportName' : sportName
-            } 
-        }).then(response => {
-            //console.log(response.data)
-            setTeams(response.data.teams);
+        // console.log(tourn1)
+        // console.log(sportName)
+        settourn(tourn1)
+        setsport(sportName)
+        axiosInstance.get(`/${tourn1.tournament_id}/${sportName}/teams`).then(response => {
+            console.log(response.data.teams)
+            setTeams(response.data.teams.filter(team => {
+                return team.status==='REGISTERED'
+            }));
         }).catch(err => console.log(err));
 
-        setActiveStep(activeStep + 1);
-    }
-
-    const handleNext = () => {
         setActiveStep(activeStep + 1);
     }
 
@@ -90,15 +77,34 @@ const AddMatch = () => {
         setActiveStep(activeStep - 1);      
     };
 
+    const onSubmit = () => {
+        console.log(mTeams, Teams.filter(team => team.team_name===mTeams[0]))
+        let id1 = Teams.filter(team => team.team_name===mTeams[0])[0].team_id
+        let id2 = Teams.filter(team => team.team_name===mTeams[1])[0].team_id
+        const Match = {
+            "tournament_id": tourn.tournament_id,
+            "team1_id": id1,
+            "team2_id": id2,
+            "sportName": sport,
+            "date": time,
+            "round": round
+        }
+        console.log(Match)
+        
+        axiosInstance.post('/match', Match,{
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
+            }
+        }).then(response => setActiveStep(activeStep + 1)).catch(err => console.log(err));
+    }
+
     function getStepContent(step)
     {
         switch (step) {
         case 0:
             return <AM_Tourn Tournaments={Tournaments} handleNext={addTeams}/>;
         case 1:
-            return <AM_teams Teams={Teams} handleNext={handleNext} handleBack={handleBack}/>;
-        case 2:
-            //return <AM_time />;
+            return <AM_teams Teams={Teams} mTea={setmTeams} setTime={setTime} r={round} round={setRound} handleNext={onSubmit} handleBack={handleBack}/>;
         default:
             return(
                 <>
