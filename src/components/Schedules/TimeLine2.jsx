12442ef1758bdbcdf09fcf7fd694d9c6a58d64a9
Pavan Kumar Reddy <56450475+bot977@users.noreply.github.com>
2021-04-08 12:19:41 +0530

    Child component to set the timeline for each sport in a tournament

---
+++ src/components/Schedules/TimeLine2.jsx
@@ -0,0 +1,85 @@
+import { SportsBasketballRounded, SportsCricketRounded, SportsSoccerRounded, SportsTennisRounded } from '@material-ui/icons'
+import React from 'react'
+import {
+    Typography,
+    makeStyles,
+    Paper,
+} from '@material-ui/core';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: '6px 16px',
+    },
+    secondaryTail: {
+        backgroundColor: theme.palette.secondary.main,
+    },
+}));
+
+const TimeLine2 = (Tourn) => {
+
+    const classes = useStyles()
+
+    const icons = (sport) => {
+        if(sport==='Basketball')
+        {
+            return (<SportsBasketballRounded/>)
+        }
+        else if(sport==='Tennis')
+        {
+            return (<SportsTennisRounded/>)
+        }
+        else if(sport==='Football')
+        {
+            return (<SportsSoccerRounded/>)
+        }
+        else if(sport==='Cricket')
+        {
+            return (<SportsCricketRounded/>)
+        }
+        else if(sport==='Badminton' || sport==='Tennis' || sport==='Table Tennis')
+        {
+            return (<SportsTennisRounded/>)
+        }
+    }
+
+    //console.log(Tourn)
+
+    return (
+        <React.Fragment>
+            {Tourn.Tourn.map( match => {
+                return (
+                    <TimelineItem>
+                        <TimelineOppositeContent>
+                        <Typography variant="body2" color="textSecondary">
+                            {match.date} {match.startTime}
+                        </Typography>
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                        <TimelineDot>
+                            {icons(match.sportName)}
+                        </TimelineDot>
+                        <TimelineConnector />
+                        </TimelineSeparator>
+                        <TimelineContent>
+                        <Paper elevation={3} className={classes.paper}>
+                            <Typography variant="h6" component="h1">
+                            {match.team1.teamName} VS {match.team2.teamName}
+                            </Typography>
+                            <Typography>{match.round}</Typography>
+                        </Paper>
+                        </TimelineContent>
+                    </TimelineItem>
+                )
+            })}
+        </React.Fragment>
+    )
+}
+
+export default TimeLine2
