6b31871b44c767b4ade262418993d06870d54a23
Yash <yashyenugu@gmail.com>
2021-04-11 20:51:22 +0530

    Added snackbar for registration

--- src/components/Global/Register.jsx
+++ src/components/Global/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Container, TextField, Button, Typography, makeStyles, Avatar } from '@material-ui/core'
+import { Container, TextField, Button, Typography, makeStyles, Avatar, Snackbar} from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 import { axiosInstance } from '../../axiosInstance';
 import { useHistory } from "react-router-dom";
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -35,6 +36,7 @@ function Register() {
     const [passWord, setPassWord] = useState("");
     const [confirmPassWord, setConfirmPassWord] = useState("");
     const [validate, setValidate] = useState(true);
+    const [open, setOpen] = useState(false);
 
     function authenticate(event) {
         if (confirmPassWord === passWord) {
@@ -44,7 +46,10 @@ function Register() {
                 "username": userName,
                 "password": passWord
             }).then(response => {
-                history.push("/login")
+
+                setOpen(true)
+                setTimeout(() => { history.push("/login") }, 1000)
+
             })
                 .catch((err) => console.log(err))
         } else {
@@ -53,64 +58,75 @@ function Register() {
         event.preventDefault()
     }
 
+    function handleClose() {
+
+    }
+
 
     return (
-        <Container maxWidth="xs" className={classes.form}>
-            <div className={classes.register}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon/>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign Up!
+        <>
+            <Container maxWidth="xs" className={classes.form}>
+                <div className={classes.register}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign Up!
                 </Typography>
-                <form>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Username"
-                        name="username"
-                        autoFocus
-                        onChange={event => setUserName(event.target.value)}
-                        value={userName}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        onChange={event => setPassWord(event.target.value)}
-                        value={passWord}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="confirm-password"
-                        label="Confirm Password"
-                        type="password"
-                        onChange={event => setConfirmPassWord(event.target.value)}
-                        value={confirmPassWord}
-                        error={!validate}
-                        helperText={!validate && "Passwords do not match"}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        onClick={authenticate}
-                        className={classes.registerButton}
-                    >Register Account</Button>
-                </form>
-            </div>
-        </Container>
+                    <form>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Username"
+                            name="username"
+                            autoFocus
+                            onChange={event => setUserName(event.target.value)}
+                            value={userName}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            onChange={event => setPassWord(event.target.value)}
+                            value={passWord}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="confirm-password"
+                            label="Confirm Password"
+                            type="password"
+                            onChange={event => setConfirmPassWord(event.target.value)}
+                            value={confirmPassWord}
+                            error={!validate}
+                            helperText={!validate && "Passwords do not match"}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={authenticate}
+                            className={classes.registerButton}
+                        >Register Account</Button>
+                    </form>
+                </div>
+            </Container>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    Successfully Registered. Redirecting...
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
 
